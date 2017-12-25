245de2d0f17297d7cf65d62191fbc335256a9af0
Longears00 <Melani_y@126.com>
2017-12-25 17:09:29 +0800

    commit

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,43 +1,72 @@
 
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next)=> {
-  res.status('200').json({
-      message: 'products works',
-    });
-});
+    Product.find().exec().then(docs=> {
+        console.log(docs);
+        res.status('200').json({
+            message: 'products works',
+          });
+      }).catch(err=>console.log(err));
+
+  });
 
 router.post('/', (req, res, next)=> {
-    const product = {
+    const product = new Product({
         name: req.body.name,
         price: req.body.price,
-      };
-    res.status('201').json({
-        message: 'products works',
-        createdProduct: product,
+      });
+    product.save().then(result=> {
+        console.log(result);
+        res.status('201').json({
+            message: 'products works',
+            createdProduct: product,
+          });
+      }).catch(err=> {
+        console.log(err);
       });
   });
 
 router.get('/:id', (req, res, next)=> {
     const id = req.params.id;
-    res.status('200').json({
-        message: 'products works',
-      });
+    Product.findById(id).exec().then(doc=> {
+        console.log(doc);
+        if (doc)
+        {
+          res.status(200).json(doc);
+        } else {
+          res.status(404).json('no');
+        }
+      }).catch(err=>  {console.log(err);});
   });
 
 router.patch('/:id', (req, res, next)=> {
     const id = req.params.id;
-    res.status('200').json({
-        message: 'products works',
+    const updateOps = {};
+    for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
+    }
+
+    console.log(updateOps);
+
+    Product.update({ _id: id }, {
+        $set: { updateOps },
+      }).exec().then(res=> {
+        console.log(res);
+      }).catch(result=> {
+        console.log(result);
       });
+
   });
 
 router.delete('/:id', (req, res, next)=> {
-    const id = req.params.id;
-    res.status('200').json({
-        message: 'products works',
-      });
-  });
+      const id = req.params.id;
+      Product.remove({ _id: id }).exec().then(result=> {
+          res.status(200).json(result);
+        }).catch(err=>console.log(err));
+    });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -3,14 +3,33 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb', {
+    useMongoClient: true,
+  });
+const db = mongoose.connection;
+
+//check connection
+db.once('openUri', ()=> {
+    console.log('connection mongodb');
+  });
+
+//check db
+db.on('error', (err)=> {
+    console.log(err);
+  });
 
 const productsRouter = require('./api/routes/products');
 const orderRouter = require('./api/routes/order');
+exports.test = function (req, res) {
+  res.render('test');
+};
 
 app.use(morgan('dev'));
 
 //extend : false not allow pass rich body params
-app.use(bodyParser.urlencoded({ extend: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //middleware cross resource
@@ -22,6 +41,8 @@ app.use((req, res, next)=> {
     res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET');
     return res.status(200).json({});
   }
+
+  next();
 });
 
 //middleware products
